c2d8b00a4dc0cb460b2b3089969597db2dc20c88
TheFakeAshray <dabalnce@gmail.com>
2016-08-03 10:46:17 +1200

    bunch of old code seemed to be in the js for getcomments. Idk why, but was displaying comments twice so got rid of it and all sweet now.

--- PassPast/PassPast/Scripts/script.js
+++ PassPast/PassPast/Scripts/script.js
@@ -16,43 +16,8 @@ function viewComments(id) {
 		$(".right-sidebar-div").css("margin-top", scrollPosition + "px");
 		var commentString = "<h4 class='right-comments'>Comments:</h4>";
 		for (var i = 0; i < data.length; i++) {
-		    //console.log(data[i].DateTime);
-		    commentDateTime = new Date(parseInt(data[i].DateTime.replace("/Date(", "").replace(")/", ""), 10));
 
-		    currentDateTime = new Date();
-
-		    //console.log(currentDateTime);
-
-		    var mSecDiff = currentDateTime - commentDateTime;
-		    var secDiff = mSecDiff / 1000
-
-            var Days = Math.floor(secDiff / 60 / 60 / 24);
-            secDiff -= Days * 60 * 60 * 24;
-
-            //console.log(Days, secDiff, commentDateTime, currentDateTime); //Uncomment to check stuff ;)
-
-            var Hours = Math.floor(secDiff / 60 / 60);
-            secDiff -= Hours * 60 * 60;
-
-            var Minutes = Math.floor(secDiff / 60);
-            secDiff -= Minutes * 60;
-
-            var Seconds = Math.floor(secDiff);
-            value = Seconds.toString() + " Seconds Ago";
-            if (Minutes != 0) {
-                value = Minutes.toString() + " Minutes and " + value;
-            }
-            if (Hours != 0) {
-                value = Hours.toString() + " Hours, " + value;
-            }
-            if (Days != 0) {
-                value = Days.toString() + " Days, " + value;
-            }
-
-            value = "Posted "+value;
-
-		    commentString += "<div class='row'><div class='text-center vote-row right-vote-row col-xs-1'><form action='/Exams/AddCommentVote' method='post'><input type='hidden' name='QuestionId' value='" + id + "' /><input type='hidden' name='AnswerId' value='" + data[i].Id + "' /><input type='hidden' name='TypeOfVote' value='Up' /><input type='submit' name='submit' class='btn vote-submit-button upvote-submit-button btn-default submit-button' value='▲' /></form>" + data[i].VoteCount + "<form action='/Exams/AddCommentVote' method='post'><input type='hidden' name='QuestionId' value='" + id + "' /><input type='hidden' name='AnswerId' value='" + data[i].Id + "' /><input type='hidden' name='TypeOfVote' value='Down' /><input type='submit' name='submit' class='btn btn-default vote-submit-button downvote-submit-button submit-button' value='▼' /></form></div><div class='answer-row col-xs-10'>" + data[i].Content + " <div class='comment-timestamp'>" + value.toString() + "</div> </div></div>"
-		    //console.log(data[i].DateTime);
+		    console.log(data[i].DateTime);
             //Taking the date from the DB of the comment
 		    commentDateTime = new Date(parseInt(data[i].DateTime.replace("/Date(", "").replace(")/", ""), 10));
             //Getting the current Date/Time
