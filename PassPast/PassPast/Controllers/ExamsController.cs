69f6c8be15f2ee8494558310507e3188e816bbf9
TheFakeAshray <dabalnce@gmail.com>
2016-09-11 23:29:58 +1200

    fixed small bug adding exam

--- PassPast/PassPast/Controllers/ExamsController.cs
+++ PassPast/PassPast/Controllers/ExamsController.cs
@@ -207,7 +207,7 @@ namespace PassPast.Controllers
 			}
 			db.SaveChanges();
 
-			return RedirectToAction("Index", "Course", new { CourseCode = model.CourseCode, PaperName = model.PaperName});
+			return RedirectToAction("Index", "Exam", new { CourseCode = model.CourseCode, PaperName = model.PaperName});
         }
 
 		[HttpGet]
