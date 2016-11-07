8df20aaf9869e7c5f5a71aa92f63cc9e94540554
Fabian Wiles <fabianwiles@live.com>
2016-11-07 18:33:31 +1300

    renamed folders to have versions

---
+++ PassPast 95/PassPast/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PassPast.Data.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassPast.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public ICollection<Course> Courses { get; set; } 
+    }
+}
