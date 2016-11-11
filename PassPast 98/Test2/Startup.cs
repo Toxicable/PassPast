0bdb2158903227bb96fbca4ed34c255bae503c95
TheFakeAshray <dabalnce@gmail.com>
2016-11-11 20:28:30 +1300

    Commiting test stuff for SignalR. WhatAmIDoing?

---
+++ PassPast 98/Test2/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test2.Startup))]
+namespace Test2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
