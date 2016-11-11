9bce82ffce78794ab4ac0751258943aa8a19ad0b
Fabian Wiles <fabianwiles@live.com>
2016-11-11 18:08:42 +1300

    added domain entities and built out database

---
+++ PassPast 98/PassPast.Data/Domain/VoteEntity.cs
@@ -0,0 +1,26 @@
+﻿using OAuthAPI.Data;
+using OAuthAPI.Data.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassPast.Data.Domain
+{
+    public class VoteEntity : Entity
+    {
+        [Range(-1, 1)]
+        public int Value { get; set; }
+        public DateTimeOffset VotedAt { get; set; }
+        public string VotedById { get; set; }
+        public ApplicationUser VotedBy { get; set; }
+    }
+
+    public enum VoteType
+    {
+        Comment,
+        Answer
+    }
+}
