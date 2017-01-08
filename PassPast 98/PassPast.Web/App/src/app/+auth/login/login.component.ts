6048de1c56ec491773077479164268614ec2b7b1
Fabian Wiles <fabianwiles@live.com>
2017-01-08 22:07:32 +1300

    prototype OAuthWindow function

--- PassPast 98/PassPast.Web/App/src/app/+auth/login/login.component.ts
+++ PassPast 98/PassPast.Web/App/src/app/+auth/login/login.component.ts
@@ -4,6 +4,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertService } from '../../core';
 import { FormValidators } from 'angular-validators';
 import { OpenIdClientService } from '@toxicable/oidc';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs'
+
 
 @Component({
   selector: 'app-login',
@@ -33,6 +36,46 @@ export class LoginComponent implements OnInit {
   googleAuthorize() {
     this.oidc.login('google')
       .subscribe(x => this.alert.sendSuccess('Successfully logged in4'));
+  }
+
+  authorizeExternal(provider: string = 'google', client_id: string = environment.googleClientId, redirect_uri: string = 'http://localhost:4200/hey') {
+
+    let providerOAuthMap: { [provider: string]: string } = {
+      google: 'https://accounts.google.com/o/oauth2/auth',
+      facebook: ''
+    };
+
+    let origin = window.location.origin;
+
+    let url = providerOAuthMap[provider] + '?' +
+      'client_id=' + encodeURIComponent(client_id) +
+      '&scope=profile' +
+      '&redirect_uri=' + encodeURIComponent(redirect_uri) +
+      '&response_type=token' +
+      '&origin=' + encodeURIComponent(origin);
+
+    let oauthWindow = window.open(url, 'OAuthWindow', 'height=600,width=450');
+
+    if (window.focus) {
+      oauthWindow.focus();
+    }
+
+    let t =  Observable.interval(200)
+      .do(() => console.log('check'))
+      .map(() => {
+        try {
+          return oauthWindow.location.href;
+        } catch (error) {
+          return '';
+        }
+      })
+      .filter(responseUrl => !!responseUrl || !!oauthWindow.closed)
+      .filter(responseUrl => responseUrl.startsWith(redirect_uri) || !!oauthWindow.closed)
+      .first()
+      .do(responseUrl => oauthWindow.close());
+
+    let y = t.flatMap(rUrl => Observable.of(rUrl));
+y.subscribe(a => console.info(a))
   }
 
 }
