5455bb077a0b03949897d05626781ad05d5e5a07
Toxicable <fabianwiles@live.com>
2017-02-07 19:35:43 +1300

    updated database rules

--- PassPast Fire/src/app/models/comment.ts
+++ PassPast Fire/src/app/models/comment.ts
@@ -2,8 +2,6 @@ export interface Comment {
     $key?: string;
     questionKey: string;
     content: string;
-    voteValue: number;
-    votesSum: number;
     createdAt: string;
     createdBy: string;
 }
