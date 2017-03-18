ec6a6b2cf46dc90d66ba600014e821ae85941cd6
Toxicable <fabianwiles@live.com>
2017-03-18 15:43:20 +1300

    Added social media icons

--- PassPast Fire/src/app/app.component.ts
+++ PassPast Fire/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { PaperService } from './papers/paper.service';
 import { CourseService } from './courses/course.service';
 import { CurrentUsersService } from './core/current-users.service';
 import { AuthService } from './core/auth.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Renderer2 } from '@angular/core';
 import { AlertService } from './core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
@@ -24,6 +24,8 @@ export class AppComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    let t: Renderer2;
+
     this.auth.profile$.subscribe(profile => {
       if (profile) {
         this.analitics.setUsername(profile.displayName);
