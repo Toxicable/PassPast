2510688129cce9b735d193bff5ac611be625a75c
Toxicable <fabianwiles@live.com>
2017-03-25 15:05:54 +1300

    refactor(shared): remove need to pass in form control to FormInput

--- PassPast Fire/src/app/shared/form-input/form-input.component.ts
+++ PassPast Fire/src/app/shared/form-input/form-input.component.ts
@@ -1,13 +1,18 @@
-import { FormControl, FormGroup } from '@angular/forms';
-import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, AbstractControl } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'form-input',
   templateUrl: 'form-input.component.html'
 })
-export class FormInputComponent {
-
-  @Input() control: FormControl;
+export class FormInputComponent implements OnInit {
+  control: AbstractControl;
+  @Input() controlName: string;
   @Input() placeholder: string;
   @Input() group: FormGroup;
+
+  ngOnInit() {
+    this.control = this.group.get(this.controlName);
+  }
+
 }
